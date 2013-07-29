978835fa03d10c7cc3d41912b6e1bd282695d133
Stanislav O <oss@gkos.name>
2013-07-29 23:20:19 +0400

    An improved version of cinder::volume::emc

--- deployment/puppet/cinder/manifests/volume/emc.pp
+++ deployment/puppet/cinder/manifests/volume/emc.pp
@@ -1,8 +1,38 @@
+# 
+# A help doc and the driver from EMC: http://github.com/xing-yang/emc-openstack-cinder
+# 
+# A full sample of a usage:
+#
+# node /.../ {
+#   class { 'cinder::volume::emc':
+#     cinder_emc_config_file_path => '/etc/cinder/cinder_emc_config.xml',
+#     cinder_emc_config_file_template => 'cinder/cinder_emc_config.xml.erb',
+#     iscsi_target_prefix  => "iqn.2001-07.com.vnx",
+#     iscsi_ip_address     => '7.7.7.7',
+#     xml_storage_type     => 'super_storage',
+#     xml_ecom_server_ip   => '2.2.2.2',
+#     xml_ecom_server_port => '22222',
+#     xml_masking_view     => 'openstack',
+#     xml_user_name        => 'storage_admin',
+#     xml_user_password    => 'storage_password',
+#     cinder_emc_opts_hash => {
+#       'EMC/my_option1' => { 'value' => 'my_text1' },
+#       'EMC/my_option2' => { 'value' => 'my_text2' },
+#     },
+#   }
+# }
+
 class cinder::volume::emc (
-  $iscsi_target_prefix = "iqn.2001-07.com.vnx",
-  $iscsi_ip_address    = '192.168.255.255',
   $cinder_emc_config_file_path = '/etc/cinder/cinder_emc_config.xml',
   $cinder_emc_config_file_template  = 'cinder/cinder_emc_config.xml.erb',
+  $iscsi_target_prefix  = "iqn.2001-07.com.vnx",
+  $iscsi_ip_address     = '',
+  $xml_storage_type     = '',
+  $xml_ecom_server_ip   = '',
+  $xml_ecom_server_port = '',
+  $xml_masking_view     = '', # may be 'openstack', is only needed for VMAX/VMAXe arrays.
+  $xml_user_name        = '',
+  $xml_user_password    = '',
   $cinder_emc_opts_hash = {},
 ) {
 
@@ -11,16 +41,24 @@ class cinder::volume::emc (
   if empty($iscsi_ip_address) {
     fail('$iscsi_ip_address is not defined in cinder::volume::emc')
   }
-  if empty($iscsi_target_prefix) {
-    fail('$iscsi_target_prefix is not defined in cinder::volume::emc')
+  if empty($xml_ecom_server_ip) {
+    fail('$xml_ecom_server_ip is not defined in cinder::volume::emc')
   }
-  if empty($cinder_emc_config_file_path) {
-    fail('$cinder_emc_config_file_path is not defined in cinder::volume::emc')
+  if empty($xml_ecom_server_port) {
+    fail('$xml_xml_ecom_server_port is not defined in cinder::volume::emc')
   }
-  if empty($cinder_emc_config_file_template) {
-    fail('$cinder_emc_config_file_template is not defined in cinder::volume::emc')
+  if empty($xml_user_name) {
+    fail('$xml_user_name is not defined in cinder::volume::emc')
   }
 
+  $pkg_pywbem = $::osfamily ? {
+    'Debian' => 'python-pywbem',
+    default => 'pywbem'
+  }
+  
+  package { $pkg_pywbem:
+    ensure => present,
+  } ->
   file { $cinder_emc_config_file_path:
     ensure => present,
     content => template( $cinder_emc_config_file_template ),
@@ -39,6 +77,6 @@ class cinder::volume::emc (
   create_resources( 'cinder_config', $cinder_emc_opts_hash )
 
   if defined(Class['::openstack::cinder']) {
-     Class['cinder::volume::emc'] -> Class['openstack::cinder']
+    Class['openstack::cinder'] -> Class['cinder::volume::emc']
   }
 }
